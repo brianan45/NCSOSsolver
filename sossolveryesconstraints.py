e58490d7e3f22531398dc9ac9dbbcdfe6806cc19
brianan45 <brianan2197@gmail.com>
2025-05-28 13:08:08 -0400

    Copied non-constraint solver as starting point

--- sossolveryesconstraints.py
+++ sossolveryesconstraints.py
@@ -7,22 +7,29 @@ from poly_input import get_polynomial_from_input
 from sp_to_cp_constraint import sp_to_cp_constraint
 from get_coeffs import get_coeffs
 
+# An SOS solver that takes a set of variables, a vector v of
+# monomials in those variables, a polynomial p(x) in those variables,
+# and a set of constraints g(x)=0, and finds the SOS decomposition of
+# p(x) or the minimum lambda such that p(x) + lambda is an SOS
+
 v, vars = get_monomial_vector()
-print(v)
-print(vars)
+# print(v)
+# print(vars)
 p = get_polynomial_from_input(vars)
-print(p)
+# print(p)
 m = v.rows
 
-# Define symmetric 4x4 matrix Q with cvxpy variables
+# Define symmetric 4x4 matrix Q and lambda with cvxpy variables
 Q = cp.Variable((m, m), PSD=True)
+lm_cp = cp.Variable(name="lm_cp")
 
 # Build the expression v^T Q v symbolically
 Q_sym = sp.Matrix(Q.shape[0], Q.shape[1], lambda i, j: sp.Symbol(f'q{min(i,j)}{max(i,j)}'))
 
 # v^T * Q * v symbolic expansion
-poly_expr = (v.T * Q_sym * v)[0, 0].expand()
-print(poly_expr)
+lm_sp = sp.Symbol("lm_sp")
+poly_expr = ((v.T * Q_sym * v)[0, 0] + lm_sp).expand()
+# print(poly_expr)
 # print((Q_sym * v)[0, 0].expand())
 # print((v.T * Q_sym)[0, 0].expand())
 
@@ -31,27 +38,35 @@ poly_expr_poly = sp.Poly(poly_expr, *vars)
 target_poly = sp.Poly(p, *vars)
 
 sol = get_coeffs(poly_expr_poly,target_poly,vars)
-print("Solution:")
-print(sol)
+# print("Solution:")
+# print(sol)
 
 if not sol:
     print("No solution; try a different monomial vector")
     sys.exit()
 
+extra_subs = {lm_sp: lm_cp}
+
 cvx_eqs = []
 for sym, expr in sol.items():
     eq = sp.Eq(sym, expr)
-    cvx_eqs.append(sp_to_cp_constraint(eq, Q))
+    cvx_eqs.append(sp_to_cp_constraint(eq, Q, extra_subs=extra_subs))
+
+cvx_eqs.append(lm_cp >= 0)
 
 # Print all converted CVXPY equations
-for e in cvx_eqs:
-    print(e)
+# for e in cvx_eqs:
+#     print(e)
 
-problem = cp.Problem(cp.Minimize(1),cvx_eqs)
+problem = cp.Problem(cp.Minimize(lm_cp),cvx_eqs)
 problem.solve()
+# print(problem.status)
 
 print("Q matrix:")
 print(Q.value)
+
+print("\nLambda:")
+print(lm_cp.value)
 
 L = np.linalg.cholesky(Q.value)
 print("\n", np.round(L,4))
