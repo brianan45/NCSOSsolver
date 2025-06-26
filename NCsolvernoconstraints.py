500f052d39db5e1c5cb5d7189f006d4de748c32b
brianan45 <brianan2197@gmail.com>
2025-06-26 13:10:41 -0400

    Starting the NC solver

---
+++ NCsolvernoconstraints.py
@@ -0,0 +1,78 @@
+import sympy as sp
+import cvxpy as cp
+import numpy as np
+import sys
+from NCmonomial_input import get_vars_vec
+from NCpoly_input import get_poly
+from sp_to_cp_constraint import sp_to_cp_constraint
+from NCget_coeffs import get_coeffs
+from matrix_sqrt import matrix_sqrt
+from clean_value import clean_value
+
+# An SOS solver that takes a set of variables, a vector v of
+# monomials in those variables, a polynomial p(x) in those variables,
+# and a set of constraints g(x)=0, and finds the SOS decomposition of
+# p(x) or the minimum lambda such that p(x) + lambda is an SOS
+
+n, v, vars = get_vars_vec()
+# print(v)
+# print(vars)
+m = v.rows
+lm_sp = sp.MatrixSymbol("lm_sp", m, m)
+p = get_poly(vars) + lm_sp
+# print(p)
+
+# Define symmetric 4x4 matrix Q and lambda with cvxpy variables
+Q = cp.Variable((m, m), PSD=True)
+lm_cp = cp.Variable((m,m), name="lm_cp")
+
+# Build the expression v^T Q v symbolically
+Q_sym = sp.Matrix(Q.shape[0], Q.shape[1], lambda i, j: sp.Symbol(f'q{min(i,j)}{max(i,j)}'))
+
+# v^T * Q * v symbolic expansion
+poly_expr = v.T * Q_sym * v
+print(poly_expr)
+
+# Convert both to polynomials
+poly_expr_poly = sp.Poly(poly_expr, *vars)
+target_poly = sp.Poly(p, *vars)
+
+sol = get_coeffs(poly_expr_poly,target_poly,vars)
+# print("Solution:")
+# print(sol)
+
+if not sol:
+    print("No solution; try a different monomial vector")
+    sys.exit()
+
+extra_subs = {lm_sp: lm_cp}
+
+cvx_eqs = []
+for sym, expr in sol.items():
+    eq = sp.Eq(sym, expr)
+    cvx_eqs.append(sp_to_cp_constraint(eq, Q, extra_subs=extra_subs))
+
+cvx_eqs.append(lm_cp >= 0)
+
+# Print all converted CVXPY equations
+for e in cvx_eqs:
+    print(e)
+
+problem = cp.Problem(cp.Minimize(lm_cp),cvx_eqs)
+problem.solve(solver=cp.CVXOPT)
+
+if problem.status == "infeasible":
+    print("Problem is infeasible. Exiting.")
+    sys.exit()
+
+print("Q matrix:")
+print(clean_value(Q.value))
+
+print("\nLambda:")
+print(clean_value(lm_cp.value))
+
+Q_sqrt = matrix_sqrt(Q.value)
+vQv_sqrt = Q_sqrt.T @ v
+sos_expr = clean_value((vQv_sqrt.T * vQv_sqrt)[0,0])
+print("\n SOS decomposition:")
+print(sos_expr)
