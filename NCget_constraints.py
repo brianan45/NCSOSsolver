50262d8b8c60c8ca2cf521e49b651bac3e76927b
Brian An <brianan2197@gmail.com>
2025-07-30 13:37:41 -0400

    Removed comments

--- NCget_constraints.py
+++ NCget_constraints.py
@@ -11,16 +11,10 @@ from sympy.parsing.sympy_parser import (
 
 transformations = standard_transformations + (convert_xor,)
 
-# transformations = standard_transformations + (convert_xor,)
-
 def get_constraints(vars, n):
-    # var_dict = {str(v): v for v in vars}
     constraints = []
     constraintsI = []
-    # print([var.name for var in vars])
     var_dict = {var.name: sp.MatrixSymbol(var.name, n, n) for var in vars}
-    # for name, symbol in var_dict.items():
-    #     print(f"{name}: is_commutative = {symbol.is_commutative}")
     var_dict["Adjoint"] = sp.Adjoint
     var_dict["I"] = sp.Identity(n)  # ✅ Add identity matrix symbol
 
@@ -32,18 +26,9 @@ def get_constraints(vars, n):
 
     for expr_str in exprs:
         expr_str = process_monomials(expr_str)
-        # print(f"Processing constraint: {expr_str}")
 
         try:
-        #     # g = sp.sympify(expr, locals=var_dict)
-        #     expr = parse_expr(
-        #         expr_str,
-        #         local_dict=var_dict,
-        #         transformations=transformations,
-        #         evaluate=True
-        #     )
             expr = add_mult(expr_str, var_dict)
-            # print(f"Parsed constraint: {expr}")
 
             constraints.append(expr)
         except Exception as e:
